c0bd224ab5eac2234bc0cf147fa3140e6c902e25
Purrrpley <63091454+Purrrpley@users.noreply.github.com>
2022-01-30 16:28:37 +1100

    Convert all indentations to spaces
    Previously there was a mix of tabs and spaces, which is not great :/ Now they're all spaces

--- pronounmc-spigot/src/io/github/acekiron/pronounmc/PMCAPI.java
+++ pronounmc-spigot/src/io/github/acekiron/pronounmc/PMCAPI.java
@@ -13,81 +13,81 @@ public class PMCAPI {
     private static HashMap<UUID, List<String>> cache = new HashMap<UUID, List<String>>();
 
     private static List<String> pronouns = new ArrayList<String>(Arrays.asList(new String[] {
-    	"he", "she", "they", "it",
-    	"any", "other", "ask", "username"
+        "he", "she", "they", "it",
+        "any", "other", "ask", "username"
     }));
 
     private static JavaPlugin plugin;
     
     public static void InitPMCAPI(JavaPlugin plugin) {
-    	PMCAPI.plugin = plugin;
-		System.out.println("Initialized PronounMC API.");
+        PMCAPI.plugin = plugin;
+        System.out.println("Initialized PronounMC API.");
     }
     
     public static List<String> getAllCodes() {
-    	return pronouns;
+        return pronouns;
     }
     
     public static String getPronouns(UUID uuid) {
-    	List<String> list = fetchPronouns(uuid);
-    	
-    	if (list.size() == 0)
-    		return "Unspecified";
-    	
-    	String string = String.join("/", pronouns);
-    	return string.substring(0, 1).toUpperCase() + string.substring(1);
+        List<String> list = fetchPronouns(uuid);
+        
+        if (list.size() == 0)
+            return "Unspecified";
+        
+        String string = String.join("/", pronouns);
+        return string.substring(0, 1).toUpperCase() + string.substring(1);
     }
     
     private static List<String> fetchPronouns(UUID uuid) {
-    	if (cache.containsKey(uuid)) {
-    		return cache.get(uuid);
-    	}
-    	
-    	// Load from config
-    	@SuppressWarnings("unchecked")
-		List<String> code = (List<String>) plugin.getConfig().getList("pronouns-" + uuid);
-    	
-    	if (code == null) {
-    		code = new ArrayList<String>();
-    	}
-    	
-    	cache.put(uuid, code);
-    	return code;
+        if (cache.containsKey(uuid)) {
+            return cache.get(uuid);
+        }
+        
+        // Load from config
+        @SuppressWarnings("unchecked")
+        List<String> code = (List<String>) plugin.getConfig().getList("pronouns-" + uuid);
+        
+        if (code == null) {
+            code = new ArrayList<String>();
+        }
+        
+        cache.put(uuid, code);
+        return code;
     }
     
     public static boolean addPronouns(UUID uuid, String code) {
-    	if (pronouns.contains(code)) {
-    		List<String> pronouns = fetchPronouns(uuid);
-    		
-    		if (!pronouns.contains(code)) {
-	    		pronouns.add(code);
-	    		cache.put(uuid, pronouns);
-		    	
-		    	// Save to config
-		    	plugin.getConfig().set("pronouns-" + uuid, pronouns);
-		    	plugin.saveConfig();
-    		}
-	    	
-	    	return true;
-    	}
-    	
-    	return false;
+        if (pronouns.contains(code)) {
+            List<String> pronouns = fetchPronouns(uuid);
+            
+            if (!pronouns.contains(code)) {
+                pronouns.add(code);
+                cache.put(uuid, pronouns);
+                
+                // Save to config
+                plugin.getConfig().set("pronouns-" + uuid, pronouns);
+                plugin.saveConfig();
+            }
+            
+            return true;
+        }
+        
+        return false;
     }
     
     public static boolean removePronouns(UUID uuid, String code) {
-    	if (pronouns.contains(code)) {
-    		List<String> pronouns = fetchPronouns(uuid);
-    		pronouns.remove(code);
-    		cache.put(uuid, pronouns);
-	    	
-	    	// Save to config
-	    	plugin.getConfig().set("pronouns-" + uuid, pronouns);
-	    	plugin.saveConfig();
-	    	
-	    	return true;
-    	}
-    	
-    	return false;
+        if (pronouns.contains(code)) {
+            List<String> pronouns = fetchPronouns(uuid);
+            pronouns.remove(code);
+            cache.put(uuid, pronouns);
+            
+            // Save to config
+            plugin.getConfig().set("pronouns-" + uuid, pronouns);
+            plugin.saveConfig();
+            
+            return true;
+        }
+        
+        return false;
     }
     
 }
